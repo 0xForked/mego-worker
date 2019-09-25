12203dc17751c0dc665a4811f83b46345152aaab
aasumitro <aasumitro@gmail.com>
2019-09-25 13:04:34 +0800

    update command

--- delivery/action.go
+++ delivery/action.go
@@ -1,7 +1,6 @@
 package delivery
 
 import (
-	"fmt"
 	"github.com/aasumitro/mego-worker/data"
 	"github.com/aasumitro/mego-worker/helper"
 	"log"
@@ -18,14 +17,17 @@ func SendToDevice(outbox data.Outbox) {
 	// If the counter becomes zero, all goroutines blocked on Wait are released.
 	// If the counter goes negative, Add panics.
 	wg.Add(1)
-	// "echo '%s' | gammu --sendsms TEXT %s"
-	// Command String
-	cmd := fmt.Sprintf(
-		"echo send text '%s' to %s",
-		outbox.TextDecoded,
-		outbox.DestinationNumber)
+	// "echo '%s' | gammu --sendsms TEXT %s" - use this command or
+	// gammu sendsms TEXT 082271115593 -text "Halooo .." - this command
+	// Command String gammu sendsms TEXT %s -text '%s'
+	//cmd := fmt.Sprintf(
+	//	`gammu-smsd-inject TEXT %s -text "%s"`,
+	//	outbox.DestinationNumber,
+	//	outbox.TextDecoded)
+	cmd := "gammu-smsd-inject"
+	args := []string{"TEXT", outbox.DestinationNumber, "-text", outbox.TextDecoded}
 	// exec command with goroutine
-	go helper.ExecCommand(cmd, wg)
+	go helper.ExecCommand(cmd, args, wg)
 	// Wait blocks until the WaitGroup counter is zero.
 	wg.Wait()
 
@@ -43,26 +45,31 @@ func SendToQueueTable(outbox data.Outbox) {
 	text := len(outbox.TextDecoded)
 	// handle when message with text under 160 chars
 	if text < 160 {
-		log.Printf("inject to device")
-		// "echo '%s' | gammu-smsd-inject TEXT %s"
+		log.Printf("send message with gammu-smsd-inject")
+		// gammu-smsd-inject TEXT %s -text '%s'
 		// Command String
-		cmd := fmt.Sprintf(
-			"echo send text '%s' to %s",
-			outbox.TextDecoded,
-			outbox.DestinationNumber)
+		//cmd := fmt.Sprintf(
+		//	"gammu-smsd-inject TEXT %s -text '%s'",
+		//	outbox.DestinationNumber,
+		//	outbox.TextDecoded)
+		cmd := "gammu-smsd-inject"
+		args := []string{"TEXT", outbox.DestinationNumber, "-text", outbox.TextDecoded}
 		// exec command with goroutine
-		go helper.ExecCommand(cmd, wg)
+		go helper.ExecCommand(cmd, args, wg)
 	}
 	// handle when message have more than 160 chars
 	if text > 160 {
 		log.Printf("Handle Multipart Message")
 		// echo '%s' | gammu-smsd-inject TEXT %s -len 400
-		cmd := fmt.Sprintf(
-			"echo send text '%s' to %s",
-			outbox.TextDecoded,
-			outbox.DestinationNumber)
+		// gammu-smsd-inject TEXT %s -len 400 -text '%s'
+		//cmd := fmt.Sprintf(
+		//	"gammu-smsd-inject TEXT %s -len 400 -text '%s'",
+		//	outbox.DestinationNumber,
+		//	outbox.TextDecoded)
+		cmd := "gammu-smsd-inject"
+		args := []string{"TEXT", "-len", "400", outbox.DestinationNumber, "-text", outbox.TextDecoded}
 		// exec command with goroutine
-		go helper.ExecCommand(cmd, wg)
+		go helper.ExecCommand(cmd, args, wg)
 	}
 	// exec command with goroutine
 	wg.Wait()
--- helper/cli.go
+++ helper/cli.go
@@ -3,22 +3,24 @@ package helper
 import (
 	"fmt"
 	"os/exec"
-	"strings"
 	"sync"
 )
 
-func ExecCommand(cmd string, wg *sync.WaitGroup) {
+func ExecCommand(cmd string, args []string, wg *sync.WaitGroup) {
 	fmt.Println("Execution command: ", cmd)
+	fmt.Println("Command argument: ", args)
 	// splitting head => g++ parts => rest of the command
-	parts := strings.Fields(cmd)
-	head := parts[0]
-	parts = parts[1:len(parts)]
+	//parts := strings.Fields(cmd)
+	//head := parts[0]
+	//fmt.Println(head)
+	//parts = parts[1:len(parts)]
+	//fmt.Println(parts)
 	// exec command
-	out, err := exec.Command(head, parts...).Output()
-	// handle error
-	CheckError(err, "Failed execution cli command")
+	out, err := exec.Command(cmd, args...).Output()
 	// show callback
 	fmt.Printf("%s", out)
+	// handle error
+	CheckError(err, "Failed execution cli command")
 	// Need to signal to wait group that this goroutine is done
 	wg.Done()
 }
